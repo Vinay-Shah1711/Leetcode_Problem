1b25d4981ce649b32e44c303cf351a9db632de63
Vinay-Shah1711 <vinayshah1711@gmail.com>
2025-09-19 20:57:35 +0530

    Create minsubarray 19-09.py

---
+++ minsubarray 19-09.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        n = len(nums)
+        left = 0
+        curr_sum = 0
+        res = float("inf")  # initially infinity
+        
+        for right in range(n):
+            curr_sum += nums[right]  # expand window to the right
+            
+            # shrink window while sum is large enough
+            while curr_sum >= target:
+                res = min(res, right - left + 1)
+                curr_sum -= nums[left]
+                left += 1
+        
+        return 0 if res == float("inf") else res
